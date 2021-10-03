485cd85259e3c7db85258d53cb676538042ce1c2
hycmatthew <hycmatthew@gmail.com>
2021-10-03 23:16:56 +0800

    commit change

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import './App.scss'; 
-import { TopMenu } from './TopMenu'; 
 import { MainPage } from './MainPage.js'; 
 import { PageWindow } from './PageWindow.js'; 
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 export const App = () => {
     return(
-        <div>
-            <TopMenu />
-            <PageWindow />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/preview">
+                    <PageWindow />
+                </Route>
+                <Route exact path="/">
+                    <MainPage />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
--- react-demo/src/MainPage.js
+++ react-demo/src/MainPage.js
@@ -1,5 +1,45 @@
 import React, { useEffect } from "react";
+import { TopMenu } from './TopMenu.js'; 
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import paperImage from './paper.png';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 export function MainPage() {
-  
+
+    let cardList = [1,2,3,4,5,6,7];
+
+    function createCard(id){
+        return (
+            <Card key={id} className="image-card" sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    alt="cannot download the image"
+                    height="140"
+                    image={paperImage}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">Lizard</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Link to="/preview"><Button size="small">Learn More</Button></Link>
+                </CardActions>
+            </Card>
+        );
+    }
+
+    return (
+        <div className="web-page">
+            <TopMenu />
+            <div className="main-page">
+                { cardList.map((list) => createCard(list) )}
+            </div>
+        </div>
+    );
 }
